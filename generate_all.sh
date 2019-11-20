b91e162fb97dbf133cb540a1f190f252ff90bc59
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2019-11-20 20:06:44 +0100

    Release könyvtár összerakása

--- generate_all.sh
+++ generate_all.sh
@@ -1,5 +1,10 @@
 sh generate_midi.sh
 sh generate_wav.sh
-sh generate_ogg.sh
+#sh generate_ogg.sh
 sh generate_mp3.sh
 rm *.wav;
+rm kottanak.{mp3,midi}
+mv kottanak.pdf kotta.pdf
+rm -r release
+mkdir release
+mv *.midi *.mp3 *.pdf release/
