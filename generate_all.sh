c98a59ff67ed97e9796c5b9085c3a38e5c8efa17
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2019-12-05 22:16:18 +0100

    generate_all áttekinthetőbbé tétele

--- generate_all.sh
+++ generate_all.sh
@@ -3,7 +3,7 @@ sh generate_wav.sh
 #sh generate_ogg.sh
 sh generate_mp3.sh
 rm *.wav;
-mv kottanak.pdf kotta.pdf
 rm -r release
 mkdir release
-mv *.midi *.mp3 *.pdf release/
+mv kottanak.pdf release/kotta.pdf
+mv *.midi *.pdf *.mp3 release/
